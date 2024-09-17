6c615bdc18a6e0995df7d6832829eb421bd697e3
cimisachimi <72540063+cimisachimi@users.noreply.github.com>
2024-09-17 08:03:46 +0700

    start

---
+++ src/components/Charts/Pie.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Pie = () => {
+  return (
+    <div>
+      <div>Pie Chart</div>
+    </div>
+  );
+};
+
+export default Pie;
---
+++ src/components/Charts/Stacked.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Stacked = () => {
+  return (
+    <div>
+      <div>Stacked Chart</div>
+    </div>
+  );
+};
+
+export default Stacked;
